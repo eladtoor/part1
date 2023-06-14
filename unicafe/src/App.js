c9b8dd2b4d0c3e725a73321624d10ef739794886
Elad <eladtoordev@gmail.com>
2023-06-14 21:31:12 +0300

    step 4

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -6,16 +6,18 @@ const Button = ({ buttonName, handleButton }) => {
   return <button onClick={handleButton}>{buttonName}</button>;
 };
 const Statistics = ({ good, neutral, bad, all, average, positive }) => {
-  return (
-    <div>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {all}</p>
-      <p>average {average}</p>
-      <p>positive {positive * 100}%</p>
-    </div>
-  );
+  if (good || neutral || bad || all || average || positive)
+    return (
+      <div>
+        <p>good {good}</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
+        <p>all {all}</p>
+        <p>average {average}</p>
+        <p>positive {positive * 100}%</p>
+      </div>
+    );
+  else return <h4>No feedback given</h4>;
 };
 const App = () => {
   // save clicks of each button to its own state
